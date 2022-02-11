c23d468a0555417ef9449bfef3eb433c2cc031b2
rio.udagawa <udazemioo@gmail.com>
2022-02-11 10:37:44 +0900

    Employeeモデル、EmployeeViewモデル作成

--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import main.java.constants.JpaConst;
+import constants.JpaConst;
 
 public class DBUtil {
 
